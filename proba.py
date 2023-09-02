6efcfdcaf69c3982885efc19b3e21a9ae4425255
Wojtek <wlkr2000@gmail.com>
2023-09-02 20:25:40 +0200

    commit 2

--- proba.py
+++ proba.py
@@ -34,5 +34,5 @@ filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
-
+st.subheader('Map of all pickups at')
 #zmiana 1
